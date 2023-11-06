d2bfa9cff72d357cbd68b2ae5efeda812ee94dc9
Ceunah <ceunahkunz@gmail.com>
2023-11-06 00:05:01 -0800

    pesan commit Anda di sini

---
+++ app/info/page.tsx
@@ -0,0 +1,7 @@
+const Info = () => {
+return (
+    <h2>Info</h2>
+)
+}
+
+export default Info;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+   <a href="/about"><button style={{fontSize:"20px",margin:"20px" }}>About</button></a>
+ 
+  )
+}
